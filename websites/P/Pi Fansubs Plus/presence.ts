dfb668ccff39df61061461dbc52bdff4bb239bb1
Bruno Ramos <37979213+Kuriel23@users.noreply.github.com>
2026-07-11 17:33:02 +0100

    feat(Pi Fansubs Plus): add iframe and presence handling for video streaming

---
+++ websites/P/Pi Fansubs Plus/presence.ts
@@ -0,0 +1,173 @@
+import { ActivityType, Assets, getTimestamps } from 'premid'
+
+const presence = new Presence({
+  clientId: '1525530116317511821',
+})
+const strings = presence.getStrings({
+  playing: 'general.playing',
+  paused: 'general.paused',
+  searchFor: 'general.searchFor',
+  viewHome: 'general.viewHome',
+  viewGenre: 'general.viewGenre',
+  viewPage: 'general.viewPage',
+  viewSeries: 'general.viewSeries',
+  viewMovie: 'general.viewMovie',
+  buttonViewSeries: 'general.buttonViewSeries',
+  buttonViewEpisode: 'general.buttonViewEpisode',
+  buttonWatchMovie: 'general.buttonWatchMovie',
+  viewCategory: 'general.viewCategory',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/aSgh0w8.jpeg',
+}
+
+let iFrameVideo: boolean, currentTime: number, duration: number
+
+interface IFrameData {
+  iframeVideo: {
+    iFrameVideo: boolean
+    currTime: number
+    dur: number | null
+  }
+}
+
+presence.on('iFrameData', (data: IFrameData) => {
+  if (data.iframeVideo.dur) {
+    ({ iFrameVideo } = data.iframeVideo)
+    currentTime = data.iframeVideo.currTime
+    duration = data.iframeVideo.dur
+  }
+})
+
+function textContent(tags: string) {
+  return document.querySelector(tags)?.textContent?.trim()
+}
+
+function getImage(tags: string) {
+  return document.querySelector<HTMLImageElement>(tags)?.src ?? ActivityAssets.Logo
+}
+
+presence.on('UpdateData', async () => {
+  const { pathname, href, search } = document.location
+  const path = pathname.split('/')
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+    details: (await strings).viewHome,
+    type: ActivityType.Watching,
+  }
+
+  switch (path[1]) {
+    case 'release': {
+      presenceData.details = 'Vendo lançamentos de:'
+      presenceData.state = textContent('.heading-archive')
+      break
+    }
+    case 'genre': {
+      presenceData.details = (await strings).viewGenre
+      presenceData.state = textContent('.heading-archive')
+      break
+    }
+    case 'tvshows': {
+      if (path[2] && path[2] !== 'page') {
+        presenceData.details = (await strings).viewSeries
+        presenceData.state = textContent(
+          '.sheader > div:nth-child(2) > h1:nth-child(1)',
+        )
+        presenceData.largeImageKey = getImage('.poster > img:nth-child(1)')
+        presenceData.buttons = [
+          {
+            label: (await strings).buttonViewSeries,
+            url: href,
+          },
+        ]
+      }
+      else {
+        presenceData.details = 'Explorando Séries'
+      }
+      break
+    }
+    case 'movies': {
+      if (path[2] && path[2] !== 'page') {
+        presenceData.name = textContent('.sheader > div:nth-child(2) > h1:nth-child(1)')
+        presenceData.largeImageKey = getImage('.poster > img:nth-child(1)')
+        presenceData.details = 'No Pi Fansubs Plus'
+        if (iFrameVideo && !Number.isNaN(duration)) {
+          [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(
+            Math.floor(currentTime),
+            Math.floor(duration),
+          )
+          presenceData.smallImageKey = Assets.Play
+          presenceData.smallImageText = (await strings).playing
+        }
+        else {
+          presenceData.smallImageKey = Assets.Pause
+          presenceData.smallImageText = (await strings).paused
+        }
+        presenceData.buttons = [
+          {
+            label: (await strings).buttonWatchMovie,
+            url: href,
+          },
+        ]
+      }
+      else {
+        presenceData.details = 'Explorando Filmes'
+      }
+      break
+    }
+    case 'episodes': {
+      if (path[2] && path[2] !== 'page') {
+        const match = textContent('.epih1')?.match(/(\d+)x(\d+)/)
+
+        presenceData.largeImageKey = getImage(
+          '.mark-1 > div:nth-child(1) > img:nth-child(1)',
+        )
+        presenceData.details = 'No Pi Fansubs Plus'
+        presenceData.name = textContent('.epih1')?.replace(/: \d+x\d+/, '')
+        presenceData.largeImageText = match
+          ? `Season ${match[1]}, Episode ${match[2]}`
+          : textContent('.epih1')
+        if (iFrameVideo && !Number.isNaN(duration)) {
+          [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(
+            Math.floor(currentTime),
+            Math.floor(duration),
+          )
+          presenceData.smallImageKey = Assets.Play
+          presenceData.smallImageText = (await strings).playing
+        }
+        else {
+          presenceData.smallImageKey = Assets.Pause
+          presenceData.smallImageText = (await strings).paused
+        }
+        presenceData.buttons = [
+          {
+            label: (await strings).buttonViewEpisode,
+            url: href,
+          },
+        ]
+      }
+      else {
+        presenceData.details = 'Explorando Episódios'
+      }
+      break
+    }
+    default: {
+      if (search) {
+        presenceData.details = (await strings).searchFor
+        presenceData.state = decodeURI(search.split('=')[1]!).replaceAll(
+          '+',
+          ' ',
+        )
+      }
+      else if (path[1]!.length >= 1) {
+        presenceData.details = (await strings).viewPage
+        presenceData.state = textContent('.head')
+      }
+      break
+    }
+  }
+  presence.setActivity(presenceData)
+})
